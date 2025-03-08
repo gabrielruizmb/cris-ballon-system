4e53b62ed1ed0c6440946dfd68812f3874b5c28d
Gabriel Ruiz Mussi Bersot <gabrielruizmb@gmail.com>
2025-03-07 22:03:58 -0300

    :sparkle: Adicionado CRUD de cliente

--- src/global/Menu.java
+++ src/global/Menu.java
@@ -2,6 +2,7 @@ package global;
 
 import java.util.Scanner;
 
+import customer.CustomerCrud;
 import product.ProductCrud;
 
 public class Menu {
@@ -22,6 +23,10 @@ public class Menu {
             switch (choice) {
                 case 1: 
                     productMenu();  
+                    break;
+
+                case 2:
+                    customerMenu();
                     break;
 
                 case 4: 
@@ -62,6 +67,36 @@ public class Menu {
                     ProductCrud.read();
                     break;
             
+                default:
+                    break;
+            }
+        }
+    }
+
+    private static void customerMenu() {
+        int choice = 1;
+        while (choice != 0) {
+            System.out.print("\n---SISTEMA CRIS BALLON---\n");
+            System.out.print("~~~Menu clientes~~~\n\n");
+            System.out.print(" 1.Cadastrar cliente \n 2.Editar cliente\n");
+            System.out.print(" 3.Excluir cliente \n 4.Ver clientes");
+            System.out.print(" \n\n 0.Voltar \n\n");
+            System.out.print("Escolha uma opção: ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    CustomerCrud.create();
+                    break;
+                case 2:
+                    CustomerCrud.update();
+                    break;
+                case 3:
+                    CustomerCrud.delete();
+                    break;
+                case 4:
+                    CustomerCrud.read();
+                    break;
                 default:
                     break;
             }
