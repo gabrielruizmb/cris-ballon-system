2e6074ecde75986ab9af979b8a1f1b9fca3070c9
Gabriel Ruiz Mussi Bersot <gabrielruizmb@gmail.com>
2025-03-10 15:33:23 -0300

    Adicionado ENUM para status do pedido

--- src/App.java
+++ src/App.java
@@ -1,7 +1,11 @@
+import order.OrderStatus;
 import system.Menu;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Menu.mainMenu();
+        //Menu.mainMenu();
+        for (OrderStatus status : OrderStatus.values()) {
+            System.out.printf("%d)%s \n", status.getNumber(), status.getName());
+        }
     }
 }
